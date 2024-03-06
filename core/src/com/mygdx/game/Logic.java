b38624f404192f33b404b45202b40d754b869503
imanninen <igor.manninen@gmail.com>
2024-03-06 22:31:40 +0200

    1)add filed property to Logic.java 2)add Cell class to Logic 3)add filed draw func in View.java

--- core/src/com/mygdx/game/Logic.java
+++ core/src/com/mygdx/game/Logic.java
@@ -8,6 +8,23 @@ public class Logic {
         DOWN,
     }
 
+    public enum CellType {
+        FLOR,
+        WALL,
+        ENTRANCE,
+        TREASURE
+    }
+    public static class Cell {
+        CellType type;
+        boolean hasShadow;
+
+        Cell (CellType type, boolean hasShadow) {
+            this.type = type;
+            this.hasShadow = hasShadow;
+        }
+
+    }
+
     public int getFieldWidth() {
         return fieldWidth;
     }
@@ -24,6 +41,8 @@ public class Logic {
         return playerY;
     }
 
+    public Cell[][] field;
+
     private int playerX;
     private int playerY;
     private final int fieldWidth;
@@ -32,6 +51,24 @@ public class Logic {
     public Logic(int fieldWidth, int fieldHeight) {
         this.fieldWidth = fieldWidth;
         this.fieldHeight = fieldHeight;
+        // test load field
+        this.field = new Cell[fieldHeight][fieldWidth];
+        for (int i = 0; i < field.length; i++)
+            for (int j = 0; j < field[i].length; j++){
+                if (i == 0 && j == 0) {
+                    field[i][j] = new Cell(CellType.FLOR, false);
+                    continue;
+                }
+                if (i == 1 && j == 0) {
+                    field[i][j] = new Cell(CellType.FLOR, false);
+                    continue;
+                }
+                if (i == 0 || i == field.length - 1 || j == 0) {
+                    field[i][j] = new Cell(CellType.WALL, false);
+                    continue;
+                }
+                field[i][j] = new Cell(CellType.FLOR, false);
+            }
 
         System.out.println("New game field os size (" + fieldWidth + ", " + fieldHeight + ")");
         System.out.println("Player is at (" + playerX + ", " + playerY + ")");
@@ -42,18 +79,22 @@ public class Logic {
         switch (dir) {
             case LEFT:
                 if (playerX <= 0) { break; }
+                if (field[playerY][playerX - 1].type == CellType.WALL) {break;}
                 playerX -= 1;
                 break;
             case RIGHT:
                 if (playerX + 1 >= fieldWidth) { break; }
+                if (field[playerY][playerX + 1].type == CellType.WALL) {break;}
                 playerX += 1;
                 break;
             case UP:
                 if (playerY <= 0) { break; }
+                if (field[playerY - 1][playerX].type == CellType.WALL) {break;}
                 playerY -= 1;
                 break;
             case DOWN:
                 if (playerY + 1 >= fieldHeight) { break; }
+                if (field[playerY + 1][playerX].type == CellType.WALL) {break;}
                 playerY += 1;
                 break;
         }
