eaee95dafee0c5b1789b299efe8dda1fcf72087a
VenInf <george30032002@gmail.com>
2024-03-23 15:36:21 +0300

    + map and tileset examples

---
+++ assets/tileset.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="assets/tileset" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="tileType" propertytype="TileTypeEnum" value="FLOOR"/>
+  </properties>
+  <image width="64" height="64" source="grass.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="tileType" propertytype="TileTypeEnum" value="WALL"/>
+  </properties>
+  <image width="64" height="64" source="wall.jpg"/>
+ </tile>
+</tileset>
