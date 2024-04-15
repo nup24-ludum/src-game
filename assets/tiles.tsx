a4a9e2096498183aec8be63ac12a6a6c2d2e4b8a
InnocentusLime <innocentuslime.help@gmail.com>
2024-04-15 23:22:01 +0300

    Add tiled project

---
+++ assets/tiles.tsx
@@ -0,0 +1,150 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="100" tileheight="100" tilecount="24" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="LevelTile">
+  <properties>
+   <property name="is_walkable" type="bool" value="true"/>
+   <property name="java_ty" propertytype="ty" value="BATH_FLOOR"/>
+  </properties>
+  <image width="100" height="100" source="bathroom.png"/>
+ </tile>
+ <tile id="1" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="BED_BOT"/>
+  </properties>
+  <image width="100" height="100" source="bed_bot.png"/>
+ </tile>
+ <tile id="2" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="BED_TOP_B"/>
+  </properties>
+  <image width="100" height="100" source="bed_up_b.png"/>
+ </tile>
+ <tile id="3" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="BED_TOP_E"/>
+  </properties>
+  <image width="100" height="100" source="bed_up_e.png"/>
+ </tile>
+ <tile id="4" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="BED_TOP_G"/>
+  </properties>
+  <image width="100" height="100" source="bed_up_g.png"/>
+ </tile>
+ <tile id="5" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="BED_TOP_G2"/>
+  </properties>
+  <image width="100" height="100" source="black-girl.png"/>
+ </tile>
+ <tile id="6" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="BR"/>
+  </properties>
+  <image width="100" height="100" source="br.png"/>
+ </tile>
+ <tile id="7" type="LevelTile">
+  <properties>
+   <property name="is_walkable" type="bool" value="true"/>
+  </properties>
+  <image width="100" height="100" source="floor.png"/>
+ </tile>
+ <tile id="8" type="LevelTile">
+  <properties>
+   <property name="is_walkable" type="bool" value="true"/>
+   <property name="java_ty" propertytype="ty" value="TOILET_FLOOR"/>
+  </properties>
+  <image width="100" height="100" source="floor_toilet.png"/>
+ </tile>
+ <tile id="9" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="BL"/>
+  </properties>
+  <image width="100" height="100" source="pixil-frame-0(24).png"/>
+ </tile>
+ <tile id="10" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="SHOWER_L"/>
+  </properties>
+  <image width="100" height="100" source="shower_l.png"/>
+ </tile>
+ <tile id="11" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="SHOWER_R"/>
+  </properties>
+  <image width="100" height="100" source="shower_r.png"/>
+ </tile>
+ <tile id="12" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="SINK_L"/>
+  </properties>
+  <image width="100" height="100" source="sink_l.png"/>
+ </tile>
+ <tile id="13" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="SINK_R"/>
+  </properties>
+  <image width="100" height="100" source="sink_r.png"/>
+ </tile>
+ <tile id="14" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="TOILET"/>
+  </properties>
+  <image width="100" height="100" source="toilet.png"/>
+ </tile>
+ <tile id="15" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="TOILET_L"/>
+  </properties>
+  <image width="100" height="100" source="toilet_l.png"/>
+ </tile>
+ <tile id="16" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="TOILET_M"/>
+  </properties>
+  <image width="100" height="100" source="toilet_m.png"/>
+ </tile>
+ <tile id="17" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="TOILET_R"/>
+  </properties>
+  <image width="100" height="100" source="toilet_r.png"/>
+ </tile>
+ <tile id="18" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="TUMB"/>
+  </properties>
+  <image width="100" height="100" source="tumb.png"/>
+ </tile>
+ <tile id="19" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="TUMB_COOKIE"/>
+  </properties>
+  <image width="100" height="100" source="tumb_cookie.png"/>
+ </tile>
+ <tile id="20" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="UL"/>
+  </properties>
+  <image width="100" height="100" source="ul.png"/>
+ </tile>
+ <tile id="21" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="UR"/>
+  </properties>
+  <image width="100" height="100" source="ur.png"/>
+ </tile>
+ <tile id="22" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="UR_CANDIES"/>
+  </properties>
+  <image width="100" height="100" source="ur_candies.png"/>
+ </tile>
+ <tile id="23" type="LevelTile">
+  <properties>
+   <property name="java_ty" propertytype="ty" value="WALL"/>
+  </properties>
+  <image width="100" height="100" source="wall.png"/>
+ </tile>
+</tileset>
